7f5dc41bfef6c21172616efaefa1cc77b2c80c99
akiko <akikoyokota.0914@gmail.com>
2011-10-26 13:54:31 +0900

    try fixing LoadError on Heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # Filters added to this controller apply to all controllers in the application.
 # Likewise, all the methods added will be available for all controllers.
-require 'Youroom' #TODO ©“®ƒ[ƒh‚³‚ê‚È‚¢H
+require 'Youroom'
 
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
